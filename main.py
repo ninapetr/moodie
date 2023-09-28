f1f170ed5c8cbe1f1f8643b4eadbb5ddd92313f2
Yanina Petrova <yaninapetrova63@gmail.com>
2023-09-28 18:01:24 +0200

    added sentiment analysis model

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pyaudio
 import numpy as np
 import subprocess
 import json
+
 from vosk import Model, KaldiRecognizer
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
+
 
 FRAME_RATE = 16000
 CHANNELS = 1
@@ -14,6 +17,11 @@ mic = pyaudio.PyAudio()
 model = Model(model_name="vosk-model-en-us-0.22")
 rec = KaldiRecognizer(model, FRAME_RATE)
 
+# tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+# model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+emotion = pipeline('sentiment-analysis', 
+                    model='arpanghoshal/EmoRoBERTa')
+
 stream = mic.open(channels = CHANNELS,
                 format = AUDIO_FORMAT,
                 rate = FRAME_RATE,
@@ -21,9 +29,13 @@ stream = mic.open(channels = CHANNELS,
                 frames_per_buffer=FRAMES_PER_BUFFER)
 
 stream.start_stream()
+print("Listening...")
 
 while True:
     data = stream.read(4096)
     if rec.AcceptWaveform(data):
         result = rec.Result()
         text = json.loads(result)["text"]
+        print(text)
+        emotion_labels = emotion(text)
+        print(emotion_labels)
